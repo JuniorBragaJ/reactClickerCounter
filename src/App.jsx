7e9fa585d701091516922e80d9c54a7d896a3a60
Junior Braga <jpbraga.junior@gmail.com>
2022-03-15 18:48:50 -0300

    ClickerCounter

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import { useTimer } from 'react-timer-hook'; // https://www.npmjs.com/package/react-timer-hook
+import './App.css'
+
+
+function MyClickerCounter({ expiryTimestamp }) {
+  const {
+    seconds,
+    start,
+    restart,
+  } = useTimer({ expiryTimestamp, autoStart: false, onExpire: () => console.warn('onExpire called') });
+
+
+  const handlePoints = (count) => {
+    (count !== 0); {
+      setPoints(points.concat(count))
+    }
+  }
+
+  const [count, setCount] = useState(0)
+  const [points, setPoints] = useState([])
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Contador de cliques </h1>
+
+      <div className="App">
+        <button
+          onClick={() => {
+            setCount(count + 1)
+            start()
+          }}
+          disabled={seconds == 0 ? true : false}
+        >
+          Clique aqui
+        </button>
+
+        <button onClick={() => {
+          setCount(0)
+          const time = new Date();
+          time.setSeconds(time.getSeconds() + 10);
+          restart(time, false) // restart() primeiro argumento novo tempo, segundo boolean dizendo se ja reinicia contando o tempo ou não
+          handlePoints(count)
+        }}>
+          Resetar
+        </button>
+
+        <div style={{ fontSize: '40px' }}>{`${seconds} Segundo(s) restantes`}</div>
+
+        <h2 style={{ fontSize: '40px' }}>Cliques em 10 segundos: {count}</h2>
+        <h3 style={{ fontSize: '30px' }}>Cliques por minuto: {count * 6}</h3>
+        <BestScore points={[...points]}></BestScore>
+      </div>
+
+    </div>
+  );
+}
+
+function BestScore({ points }) {
+  return (
+    <>
+      <ul> <span style={{ fontSize: '25px' }}>PONTUAÇÃO:</span>
+        {points.map((score, index) => {
+          return (
+            <div style={{ fontSize: '20px' }} key={index}>
+              <span key={index}>{score}</span>
+            </div>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default function App() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 10);
+  return (
+    <div>
+      <MyClickerCounter expiryTimestamp={time} />
+    </div>
+  );
+}
